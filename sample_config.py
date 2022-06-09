4e56e830f95a2f5560230002a4d634034cbe6704
Badsha323 <98164078+Badsha323@users.noreply.github.com>
2022-06-09 16:30:31 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,3 +3,4 @@ import os
 
 class Config(object):
     RemoveBG_API = os.environ.get("RemoveBG_API", "fEJMC1Dy32Fs56gbWJKr4zkB")
+    GPLINKS_API = os.environ.get("GPLINKS_API", "010176c58eb42c1770c942276ab1fa0800edb483")
